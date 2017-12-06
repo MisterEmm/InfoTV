e3962e92385db48ff4ae942acadcb56da3901241
Martin Mander <mwmander@gmail.com>
2017-12-06 20:03:37 +0000

    Add files via upload

---
+++ PIR.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+
+import os
+
+from time import sleep
+
+from gpiozero import MotionSensor
+
+
+
+pir = MotionSensor(4)
+
+
+while True:
+    
+	if pir.motion_detected:
+        
+		os.system('sudo xscreensaver-command -deactivate &')
+        
+		sleep(15)
+    
+	else:
+        
+		sleep(0.1)
